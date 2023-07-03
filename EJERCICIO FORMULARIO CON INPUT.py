3e82f12841d3d42a5d86aae33dba1787e8fcdaec
christian23781 <138171096+christian23781@users.noreply.github.com>
2023-07-02 20:08:03 -0500

    Add files via upload

---
+++ EJERCICIO FORMULARIO CON INPUT.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 30 21:12:03 2023
+
+@author: Christian
+"""
+
+nombre= input("Ingrese Nombre:")
+print(nombre)
+apellido= input("Ingrese Apellido:")
+print(apellido)
+ubicación= input("Ingrese la ubicación:")
+print(ubicación)
+edad= input("Ingrese edad:")
+print(edad)
+
